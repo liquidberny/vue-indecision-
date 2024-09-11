d34f0b5a5698e132bd3691d0f56c7f7c68cdeeac
liquidberny <bernardo.urquijo.bu@gmail.com>
2024-09-10 21:48:19 -0700

    Message box prueba unitaria

--- tests/components/chat/MessageBox.test.ts
+++ tests/components/chat/MessageBox.test.ts
@@ -2,12 +2,22 @@ import MessageBox from "@/components/chat/MessageBox.vue";
 import { mount } from "@vue/test-utils";
 
 describe('<MessageBox />', () => {
-  test('renders input and button aelements correctly', () => {
     const wrapper = mount(MessageBox);
+  test('renders input and button aelements correctly', () => {
 
     expect(wrapper.html()).toMatchSnapshot();
     expect(wrapper.find('input[type="text"]').exists()).toBe(true);
     expect(wrapper.find('button').exists()).toBe(true);
     expect(wrapper.find('button svg').exists()).toBe(true);
+
   });
+
+  test('emits sendNessage event when button is clicked with message value', async () => {
+    const message = 'Hola mundo';
+    await wrapper.find('input[type="text"]').setValue(message);
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted('sendMessage')?.[0]).toEqual([message]);
+    expect((wrapper.vm as any).message).toBe('');
+  })
 });
